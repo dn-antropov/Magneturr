262bdfddb5bba6f0dd71d625312ebc2a524e4e56
umanema <dn.antropov@gmail.com>
2025-11-29 22:18:47 +0100

    add explosions

---
+++ objects/foes/explosion/scripts/explosion.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+@export var collision_shape: CollisionShape2D
+@export var explosion_size: float = 300.
+@export var lifespan: int = 15
+
+func _ready() -> void:
+	print("boom")
+	collision_shape.shape.radius = explosion_size
+	
+func _draw() -> void:
+	draw_circle(Vector2(0.,0.), explosion_size, Color(1, 0, 0, 1))
+	
+func _process(_delta: float) -> void:
+	if lifespan <= 0:
+		self.queue_free()
+	lifespan -= 1
--- objects/foes/mine/scripts/mine.gd
+++ objects/foes/mine/scripts/mine.gd
@@ -1,1 +1,22 @@
 extends Floatable
+
+@export var detonation_impulse: float = 2000000.
+const _explosion = preload("res://objects/foes/explosion/explosion.tscn")
+
+func _ready() -> void:
+	super._ready()
+	contact_monitor = true
+	max_contacts_reported = 4
+	body_entered.connect(_on_body_entered)
+	
+func _on_body_entered(_body: Node2D) -> void:
+	return
+	
+func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
+	for i in range(state.get_contact_count()):
+		if state.get_contact_impulse(i).length() >= detonation_impulse:
+			var explosion = _explosion.instantiate()
+			get_parent().add_child(explosion)
+			explosion.global_position = self.global_position
+			self.queue_free()
+			
